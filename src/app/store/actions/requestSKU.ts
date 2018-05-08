5a87cd68f4d4df14f1dbb2ce15d8ffa34e04ec8c
nmde <94819@protonmail.com>
2018-05-08 13:34:14 -0400

    Toolkit v0.58.8
    - requestSKU now uses the cache

--- src/app/store/actions/requestSKU.ts
+++ src/app/store/actions/requestSKU.ts
@@ -1,5 +1,7 @@
 import fs from 'fs';
+import read from 'read-big-file';
 import request from 'request';
+import SKUDictionary from '../../classes/interfaces/skuDictionary';
 import SKURequestPayload from '../../classes/interfaces/skuRequestPayload';
 import SKUSearchResults from '../../classes/interfaces/tcgplayer/skuSearchResults';
 
@@ -9,37 +11,66 @@ import SKUSearchResults from '../../classes/interfaces/tcgplayer/skuSearchResult
  * @returns {Promise<number>} A promise that resolves with the SKU
  */
 export default function requestSKU({ state }, payload: SKURequestPayload): Promise<number> {
-  return new Promise((resolve, reject) => {
-    request.get(
-      {
-        url: `http://api.tcgplayer.com/catalog/products?categoryId=1&groupId=${payload.groupId}&productName=${encodeURIComponent(payload.name)}`,
-        auth: {
-          bearer: state.accessToken,
+  return new Promise(async (resolve, reject) => {
+    const fromCache = <SKUDictionary>await (
+      () => new Promise(
+        (resolveSKUs) => {
+          console.log('Attempting to read SKUs from cache...');
+          read('cache/skus.json', true)
+            .then((skus) => {
+              resolveSKUs(skus);
+            })
+            .catch((err) => {
+              console.warn(`Error reading cache/skus.json: ${err}`);
+              resolveSKUs({});
+            });
         },
-      },
-      (err, res, body) => {
-        if (err) {
-          reject(err);
-        } else {
-          try {
+      )
+    )();
+    if (fromCache[payload.groupId] && fromCache[payload.groupId][payload.name]) {
+      console.log('Using SKU from cache');
+      resolve(fromCache[payload.groupId][payload.name]);
+    } else {
+      console.log('SKU not found in cache. Requesting TCGPlayer...');
+      request.get(
+        {
+          url: `http://api.tcgplayer.com/catalog/products?categoryId=1&groupId=${payload.groupId}&productName=${encodeURIComponent(payload.name)}`,
+          auth: {
+            bearer: state.accessToken,
+          },
+        },
+        (err, res, body) => {
+          if (err) {
+            // Fatal error: No SKU can be retrieved
+            console.error(`Error requesting SKU: ${err}`);
+            reject(err);
+          } else {
             const results: SKUSearchResults = JSON.parse(body);
+            // @TODO: Handle multiple results (is this possible?)
             if (results.totalItems === 1) {
-              fs.writeFile('cache/skus.json', JSON.stringify(results), (err) => {
-                if (err) {
-                  // @TODO
-                  console.error(err);
-                }
-                // @TODO
-                resolve(results.results[0].productConditions[0].productConditionId);
-              });
-            } else {
-              // @TODO
+              // @TODO: Different conditions (picks near mint automatically)
+              const sku = results.results[0].productConditions[0].productConditionId;
+              if (!fromCache[payload.groupId]) {
+                fromCache[payload.groupId] = {};
+              }
+              fromCache[payload.groupId][payload.name] = sku;
+              console.log('Writing new SKU to cache...');
+              fs.writeFile(
+                'cache/skus.json',
+                JSON.stringify(fromCache),
+                (err) => {
+                  if (err) {
+                    console.warn(`Error writing to cache: ${err}`);
+                  }
+                  // Nonfatal error, we can still resolve the SKU
+                  console.log('Success');
+                  resolve(sku);
+                },
+              );
             }
-          } catch (e) {
-            reject(e);
           }
-        }
-      },
-    );
+        },
+      );
+    }
   });
 }
